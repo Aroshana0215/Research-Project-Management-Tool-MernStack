31c82fcf12b3963410fe5f0c1cbb184111053c66
aroshana <rasitharoshana0215@gmail.com>
2022-04-10 02:31:54 +0530

    server file

--- Back-end/server.js
+++ Back-end/server.js
@@ -2,3 +2,38 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const express = require("express");
+//const { notFound, errorHandler} = require("./middlewares/errormiddleware");
+
+require("dotenv").config();
+const app = express();
+
+//const PORT = process.env.PORT || 8070;
+const PORT = process.env.PORT || 5006;
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+const URL = process.env.MONGOBD_URL;
+
+mongoose.connect(URL, {
+  //useCreateIndex:true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  //useFindAndModify:false
+});
+
+//app.use(notFound);
+//app.use(errorHandler);
+
+const connction = mongoose.connection;
+connction.once("open", () => {
+  console.log("Mongodb Connection success");
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port : ${PORT}`);
+});
