7b4ab2fa6ff0f223dc6288b279644497586efc78
aroshana <rasitharoshana0215@gmail.com>
2022-05-27 19:51:18 +0530

    code format changes

--- clients/src/components/Headers/Header.js
+++ clients/src/components/Headers/Header.js
@@ -1,41 +1,48 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
-//import './header.scss'
+import React from "react";
+import { Link } from "react-router-dom";
+// import "./header.scss";
 // import {useSelector} from 'react-redux'
 // import axios from 'axios'
 
-
-
 function Header() {
-    // const auth = useSelector(state => state.auth)
-    // const {user , isLogged} = auth
+  // const auth = useSelector(state => state.auth)
+  // const {user , isLogged} = auth
 
-    // const userLink = () =>{
-    //     return <li>
-    //         <Link>
-    //             <img src = {user.avatar} alt=""/> {user.name}
-    //         </Link>
-    //     </li>
-    // }
+  // const userLink = () =>{
+  //     return <li>
+  //         <Link>
+  //             <img src = {user.avatar} alt=""/> {user.name}
+  //         </Link>
+  //     </li>
+  // }
 
   return (
-      <header>
-          <div className='logo'>
-              <h1><Link to="/" >Research Management Tool</Link></h1>
-          </div>
+    <header>
+      <div className="logo">
+        <h1>
+          <Link to="/">Research Management Tool</Link>
+        </h1>
+      </div>
 
-          <ul>
-              <li><Link to="/"><i className='fas fa-shopping-cart' ></i>Cart</Link></li>
-              {/* {
+      <ul>
+        <li>
+          <Link to="/">
+            <i className="fas fa-shopping-cart"></i>Cart
+          </Link>
+        </li>
+        {/* {
                   isLogged
                   ? userLink()
                   : <li><Link to="/login"><i className='fas fa-user' ></i>Login</Link></li>
               } */}
-              <li><Link to="/login"><i className='fas fa-user' ></i>Login</Link></li>
-              
-          </ul>
-      </header>
-  ); 
+        <li>
+          <Link to="/login">
+            <i className="fas fa-user"></i>Login
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
