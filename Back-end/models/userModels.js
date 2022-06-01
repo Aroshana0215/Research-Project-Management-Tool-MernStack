872468bfad011d53ead95424500dacad3a57f35e
aroshana <rasitharoshana0215@gmail.com>
2022-06-01 21:28:33 +0530

    add new attributes user model

--- Back-end/models/userModels.js
+++ Back-end/models/userModels.js
@@ -1,35 +1,47 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true , "Please enter your name !"],
-        trim: true
+      type: String,
+      required: [true, "Please enter your name !"],
+      trim: true,
     },
 
     email: {
-        type: String,
-        required: [true , "Please enter your email !"],
-        trim: true,
-        unique: true
+      type: String,
+      required: [true, "Please enter your email !"],
+      trim: true,
+      unique: true,
     },
 
     password: {
-        type: String,
-        required: [true , "Please enter your password !"]
+      type: String,
+      required: [true, "Please enter your password !"],
     },
 
     role: {
-        type: Number,
-        default: 0 // 0 = user , 1 = admin
+      type: Number,
+      default: 0, // 0 = user , 1 = admin
     },
 
     avatar: {
-        type: String,
-        default : "https://res.cloudinary.com/af-project/image/upload/v1651039444/avatar/51f6fb256629fc755b8870c801092942_al0mka.png"
-    }
-} , {
-    timestamps: true
-})
+      type: String,
+      default:
+        "https://res.cloudinary.com/af-project/image/upload/v1651039444/avatar/51f6fb256629fc755b8870c801092942_al0mka.png",
+    },
+    havingGroup: {
+      type: Boolean,
+      default: false,
+    },
+    isLeader: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("Users" , userSchema)
+module.exports = mongoose.model("Users", userSchema);
